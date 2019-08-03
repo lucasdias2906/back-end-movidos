2fdb881c30e804c6ad082ed5e96097cd25ebfb18
Lucas <lucasdias2906@gmail.com>
2019-08-02 23:11:33 -0300

    retray connection with mongodb

--- src/app.js
+++ src/app.js
@@ -9,8 +9,32 @@ const app = express();
 const router = express.Router();
 
 //Connection with database
-const URL_CONNECT = "mongodb+srv://lucas:lucassousa@tranningapi-hbqnj.mongodb.net/dbmoved?retryWrites=true"
-mongoose.connect(URL_CONNECT);
+// const URL_CONNECT = "mongodb+srv://lucas:lucassousa@tranningapi-hbqnj.mongodb.net/dbmoved?retryWrites=true"
+// mongoose.connect(URL_CONNECT);
+
+
+
+
+
+
+const options = {
+    autoIndex: false, // Don't build indexes
+    reconnectTries: 30, // Retry up to 30 times
+    reconnectInterval: 500, // Reconnect every 500ms
+    poolSize: 10, // Maintain up to 10 socket connections
+    // If not connected, return errors immediately rather than waiting for reconnect
+    bufferMaxEntries: 0
+  }
+
+const connectWithRetry = () => {
+  mongoose.connect(URL_CONNECT, options).then(()=>{
+    console.log('MongoDB is connected')
+  }).catch(err=>{
+    console.log('MongoDB FALHA AO SE CONECTAR, retry after 5 seconds.')
+    setTimeout(connectWithRetry, 5000)
+  })
+}
+
 
 //Models
 
